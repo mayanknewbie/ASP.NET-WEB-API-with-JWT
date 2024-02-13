53fbd2698ef3d9ef9754934400c973846e6f28e9
Developer Yolohealth <developer@yolohealth.in>
2024-02-13 23:44:21 +0530

    Add project files.

---
+++ JWT_Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using JWT_Demo.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace JWT_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetEmployees")]
+        public string GetEmployees()
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("EmployeeCon").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM EmployeeData", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<Employee> EmpList = new List<Employee>();
+            Response response = new Response();
+            if(dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Employee emp = new Employee();
+                    emp.Id = Convert.ToInt32(dt.Rows[i]["ID"]);
+                    emp.Name = Convert.ToString(dt.Rows[i]["Name"]);
+                    EmpList.Add(emp);
+                }
+            }
+            if(EmpList.Count > 0)
+            {
+                return JsonConvert.SerializeObject(EmpList);
+            } else
+            {
+                response.StausCode = 100;
+                response.ErrorMessage = "Data Not Found";
+                return JsonConvert.SerializeObject(response);
+            }
+            //return "Get Employees";
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users user)
+        {
+            return "User added with username " + user.Username;
+        }
+    }
+}
---
+++ JWT_Demo/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JWT_Demo.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JWT_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if(user.Username == "admin" && user.Password == "12345")
+            {
+                _user = new Users { Username = "Oprezo India"};
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],null,expires: DateTime.Now.AddMinutes(1),signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);     
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if(user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
